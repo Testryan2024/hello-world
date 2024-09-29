61cb0b637558f2bb89ed913b0bc5ce1f6667f95d
Testryan2024 <testryan2024@outlook.com>
2024-09-29 12:37:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let $http = require("node:http");
 let $my_server = $http.createServer(($_req, $_res) => {
-    $_res.end($_req.url);
+    $_res.end($_req.headers.host);
 });
 $my_server.listen(8000);
